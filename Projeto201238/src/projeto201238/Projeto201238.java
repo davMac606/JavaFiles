6da728c752509f8a16f0c484bdd3f0044a2584b3
davMac606 <102124577+davMac606@users.noreply.github.com>
2022-05-31 17:18:03 -0300

    fvck manual commit

--- Projeto201238/src/projeto201238/Projeto201238.java
+++ Projeto201238/src/projeto201238/Projeto201238.java
@@ -1,16 +1,15 @@
 package projeto201238;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Projeto201238 {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-     Cliente cl1 = new Cliente();
-     cl1.setNome(teclado.nextLine());
-     cl1.setIdade(teclado.nextInt());  
-     cl1.setCodigo(teclado.nextInt());
-     cl1.setEmail(teclado.nextLine());
-     cl1.setTelefone(teclado.nextInt());
+     Cliente cl1 = new Cliente(teclado.nextLine(),teclado.nextInt(),teclado.nextInt(), teclado.nextInt(),teclado.nextInt());
+
+     cl1.exibir();
     }
     
 }
