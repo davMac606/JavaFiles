2e040abc6ae38822fa74a571b02cd3a429bc966c
davMac606 <102124577+davMac606@users.noreply.github.com>
2022-05-31 17:36:01 -0300

    Update Projeto201238.java

--- Projeto201238/src/projeto201238/Projeto201238.java
+++ Projeto201238/src/projeto201238/Projeto201238.java
@@ -6,10 +6,25 @@ import java.util.ArrayList;
 public class Projeto201238 {
 
     public static void main(String[] args) {
+        ArrayList<Cliente> listaCliente = new ArrayList();
         Scanner teclado = new Scanner(System.in);
-     Cliente cl1 = new Cliente(teclado.nextLine(),teclado.nextInt(),teclado.nextInt(), teclado.nextInt(),teclado.nextInt());
+     Cliente cl1 = new Cliente();
+        System.out.println("Digite o nome do cliente.\n");
+     cl1.setNome(teclado.nextLine());
+        System.out.println("Digite a idade do cliente.\n");
+     cl1.setIdade(teclado.nextInt());
+        System.out.println("Digite o código do cliente.\n");
+     cl1.setCodigo(teclado.nextInt());
+        System.out.println("Digite o número de pedidos do cliente.\n");
+     cl1.setPedidos(teclado.nextInt());
+        System.out.println("Digite o número de telefone do cliente.\n");
+     cl1.setTelefone(teclado.nextInt());
 
+     listaCliente.add(cl1);
      cl1.exibir();
+     
     }
     
 }
+
+    
